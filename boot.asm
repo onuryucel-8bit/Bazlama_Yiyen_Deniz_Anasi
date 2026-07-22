043008575c31928f283db5b2c85cec4bde80e920
unknown <onuryucel200@hotmail.com>
2026-07-22 15:17:50 +0300

    v0.2| 1 :: Kesmeler ve Klavye Girdisi

--- boot.asm
+++ boot.asm
@@ -1,10 +1,17 @@
 org 0x7c00
 bits 16
 
+.main:
+
+mov ah, 0x0     ;klavye modu
+int 0x16        ;klavye => al(ascii) karakter
+
 mov ah, 0x0e    ;tty mod
-mov al, 'A'
 int 0x10
 
+jmp .main
+
+jmp $
 
 times 510 - ($ - $$) db 0
 dw 0xAA55
